b3e9fd6d3fe85fe14c0a7d8bed4b94f21830041b
HarshY10 <131291758+HarshY10@users.noreply.github.com>
2024-04-14 15:45:41 +0530

    Update mapping_streamlit.py

--- mapping_streamlit.py
+++ mapping_streamlit.py
@@ -5,10 +5,12 @@ from geopy.geocoders import Nominatim
 import folium
 from streamlit_folium import st_folium
 
+#App Title
 st.header(':blue[Creditor/Debtor Address Mapping] :world_map:', divider='rainbow')
-uploaded_file = st.file_uploader("Upload an excel file")
 
+uploaded_file = st.file_uploader("Upload an excel file") #Excel File uploader
 
+#Reading the Excel file, storing the name and address columns, dropping the rest 
 if uploaded_file is not None:
   df = pd.read_excel(uploaded_file)
   df = df.drop(['$Parent','$Address[2].Address','$BillCreditPeriod','$LEDSTATENAME'],axis=1)
@@ -18,13 +20,12 @@ if uploaded_file is not None:
   addresses = df['$Address[1].Address'].tolist()
   names = df['$Name'].tolist()
 
-  
 
   # Create lists to store the geocoded locations and names
   locations = []
   add_name = []
 
-  @st.cache_data # caching decorator
+  @st.cache_data # caching decorator, to handle data while reloading
   def load_geodata(addresses,names):
     flag = len(names)#Flag to break the location loop
     i = 1
@@ -39,7 +40,7 @@ if uploaded_file is not None:
         i = i + 1
         if i == flag:
           break
-    st.success('Map is ready!')
+    st.success('Interactive Map is ready! Click on marker to display the name.')
     return(locations,add_name)
     
   locations,add_name = load_geodata(addresses,names)
@@ -48,9 +49,9 @@ if uploaded_file is not None:
   map = folium.Map(location=(locations[0].latitude, locations[0].longitude), zoom_start=12)
 
   for (location,name) in zip(locations,add_name):
-  # Add a marker for  location
+  # Add a marker for location along with popups to display name
     folium.Marker([location.latitude,location.longitude],
                   popup=name).add_to(map)
   
   # Display the map
-  st_folium(map, width=700)
+  st_folium(map, width=1000)
