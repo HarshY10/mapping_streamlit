e8e0966ce9101cfab491317c0491086ee0a082b6
HarshY10 <131291758+HarshY10@users.noreply.github.com>
2024-05-28 12:04:10 +0530

    Update mapping_streamlit.py

--- mapping_streamlit.py
+++ mapping_streamlit.py
@@ -11,8 +11,8 @@ st.header(':blue[Creditor/Debtor Address Mapping] :world_map:', divider='rainbow
 uploaded_file = st.file_uploader("Upload an excel file") #Excel File uploader
 
 
-@st.cache_data # caching decorator, to handle data while reloading
-def_loc = [12.9915, 80.2337] 
+#@st.cache_data # caching decorator, to handle data while reloading
+#def_loc = [12.9915, 80.2337] 
 def_map = folium.Map([12.9915, 80.2337], zoom_start=12) #default map with location IIT Madras 12.9915° N, 80.2337° E
 #Setting the marker
 folium.Marker([12.9915, 80.2337], popup="IIT Madras").addto(def_map)
